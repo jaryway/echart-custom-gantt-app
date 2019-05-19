653553b33e682f7922ad4565b205a9eb31ac9811
jaryway <weiznegxun@live.cn>
2019-05-19 20:54:49 +0800

    fixup! first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import CustomGantt from "./custom-gantt";
+import CustomGantt from "./custom-gantt";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomGantt />
+    </div>
+  );
+}
+
+export default App;
